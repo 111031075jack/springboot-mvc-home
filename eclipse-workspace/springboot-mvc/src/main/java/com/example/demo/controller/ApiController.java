860f50683f385fca8d862d27a71018b11a2f3c7d
111031075jack <111031075@live.asia.edu.tw>
2025-09-04 15:00:43 +0800

    update

---
+++ eclipse-workspace/springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+//@RestController
+public class ApiController {
+	
+	@GetMapping("/api/hello")
+	public String hello() {
+		return "Hello Springboot";
+	}
+	
+}
